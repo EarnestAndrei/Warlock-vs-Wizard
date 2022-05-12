d6e22c913a47bdc122f2a6adeb3741c78e9ff38c
EarnestAndrei <100452486+EarnestAndrei@users.noreply.github.com>
2022-05-12 14:35:41 +0800

    Update Wizard.java

--- InheritanceLab-master.zip/src/Wizard.java
+++ InheritanceLab-master.zip/src/Wizard.java
@@ -2,8 +2,8 @@ public class Wizard extends Character{
     Wizard(String name,  int lv, int hp, int mp){
         super(name,lv,hp,mp);
     }
-    public void fireStorm(Character enemyCharacter){
-        System.out.println(super.characterName + "attacks " + enemyCharacter.characterName + " with Firestorm (Damage - 50)");
+    public void EarthShatter(Character enemyCharacter){
+        System.out.println(super.characterName + "attacks " + enemyCharacter.characterName + " with Earth Shattter (Damage - 50)");
         int damagePoints = 50;
         damageTarget(enemyCharacter, damagePoints);
     };
